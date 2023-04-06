f681f1c545436e24f3f7584bd6c9d28f76508a01
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-04-06 22:17:46 +0900

    나도코딩님의 Python 기본편을 수강하고 작성했던 코드
    매 개념마다 쳤었던 practice는 매번 지우고 새로쳐서 기록이 없다. 근데 퀴즈 코드나 등등 코드 파일이 아까워서 깃에 올린다. 실습했던 코드는 https://seokyung0221.tistory.com/50에 내가 모르는 개념에 한에서만 정리해서 올려놨다.(Java나 C++과 다른 점)

---
+++ PythonWorkspace/Quiz3.py
@@ -0,0 +1,18 @@
+# Quiz) 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하시오.
+
+# 예) http://naver.com
+# 규칙1 : http:// 부분은 제외 => naver.com
+# 규칙2 : 처음 만나는 점(.) 이후 부분은 제외 => naver
+# 규칙3 : 남은 글자 중 처음 세자리 + 글자 갯수 + 글자 내 'e' 갯수 + "!"로 구성
+#                     (nav)           (5)            (1)        (!)
+# 예) 생성된 비밀번호 : nav51!
+
+site = "http://naver.com"
+password = site.replace("http://", "") # 규칙 1
+
+index = password.index(".")
+password = password[:index] # 규칙 2
+
+password = password[:3] + str(len(password)) + str(password.count("e"))+"!" # 규칙 3
+print(password)
+# str을 붙여서 문자열로 바꿔주는 것 잊지말자!
---
+++ PythonWorkspace/practice_class.py
@@ -0,0 +1,13 @@
+class Unit:
+    def __init__(self):
+        print("Unit 생성자")
+
+class Flyable:
+    def __init__(self):
+        print("Flyable 생성자")
+
+class FlyableUnit(Unit, Flyable):
+    def __init__(self):
+        super().__init__()
+
+dropship = FlyableUnit() # Unit 생성자만 호출됨. super()를 사용하면
